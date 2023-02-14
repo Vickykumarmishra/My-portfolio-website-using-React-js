b00fc7351821cfce502f77ff19c723c76f072282
vicky kumar mishra <mishravicky0141@gmail.com>
2023-02-14 18:41:55 +0530

    second project added

--- myportfolio/src/components/Home.js
+++ myportfolio/src/components/Home.js
@@ -70,7 +70,7 @@ export default function Home() {
 
     </div>
     <div class="col">
-      <motion.img  whileHover={{scale:1.1}} src='portfolio_image1-green.png' className='img-thumbnail' style={{borderRadius:"50%",height:'25rem'}}></motion.img>
+      <motion.img  whileHover={{scale:1.1}} src='my photo.jpg' className='img-thumbnail' style={{borderRadius:"50%",height:'25rem'}}></motion.img>
     </div>
 
   </div>
@@ -84,10 +84,21 @@ export default function Home() {
   <div class="row">
     <div class="col" >
       <NavLink to='https://bihar-tourism-vicky.netlify.app'><motion.h3  whileHover={{color:'yellow'}} animate={{scale:1.2}} transition={{duration:0.3 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Bihar Tourism Website</motion.h3></NavLink>
-     <NavLink to='https://bihar-tourism-vicky.netlify.app'><motion.img whileHover={{scale:1.1}} src='buddha2.jpg' className='img-thumbnail' style={{marginTop:'3rem'}} alt='....'/></NavLink>
+     <NavLink to='https://bihar-tourism-vicky.netlify.app'><motion.img whileHover={{scale:1.1}} src='buddha.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',width:'30rem'}} alt='....'/></NavLink>
      
       <div className="col" style={{marginTop:'1rem'}}>
       <motion.p whileHover={{color:'yellow'}}>This is a tourism website of Bihar, state of India, where you will find the complete details of all tourist places of incredible Bihar and its cultural heritage .it has features like: Booking System, DarkMode, Bill generation,etc.</motion.p>
+    </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col" >
+      <NavLink to='https://takcart-my-shopping-app.netlify.app'><motion.h3  whileHover={{color:'yellow'}} animate={{scale:1.2}} transition={{duration:0.3 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Takcart-a shopping website</motion.h3></NavLink>
+     <NavLink to='https://takcart-my-shopping-app.netlify.app'><motion.img whileHover={{scale:1.1}} src='takcart portfolio.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem'}} alt='....'/></NavLink>
+     
+      <div className="col" style={{marginTop:'1rem'}}>
+      <motion.p whileHover={{color:'yellow'}}>This is a Shopping website using React Js, Bootstrap,Formik,Material Ui and Css. It has facilities like -Cart system,automatic Total Price Calculation</motion.p>
     </div>
     </div>
   </div>
